d09df4e9086abcdeaef69d83132d9ced7b49e860
Nikolett Hevizi <nikihevizi@gmail.com>
2023-04-25 18:29:14 +0200

    Niki

--- index.js
+++ index.js
@@ -13,41 +13,42 @@ let posX = (x.charCodeAt()-65);
 let posY = +y;
 if(board2[posX][posY] !== "" )
 {
-  displayMessage ("Choose an other field!")
+  displayMessage ("This field is already in use! Choose an other one!", "red")
 }
-else if (canPlaceThere(posX,posY))
+else if (canPlaceThere(posX,posY) && shipsPlaced < 2)
 {
-board2[posX][posY] = "X";
+board2[posX][posY] = "S";
 displayBoard({boardnumber : 2 , board : board2})
 shipsPlaced ++
+displayMessage("You have placed " + shipsPlaced + " ships")
 }
-if(shipsPlaced >= 2)
+else if(shipsPlaced >= 2)
 {
-  displayMessage ("You have finished the placement phase. Start shooting!" , "purple")
+  displayMessage ("You have finished the placement phase. Start shooting!" , "black")
   phase = "shooting"
 }
 }
 
 function canPlaceThere(x,y)
 {
-  if (x > 0 && board2[x-1][y] === "X")
+  if (x > 0 && board2[x-1][y] === "S")
   {
-    displayMessage("No!")
+    displayMessage("You can not place your ships next to each other! Choose an other field!", "red")
     return false
   }
-  else if (x < board2.length-1 && board2[x+1][y] === "X")
+  else if (x < board2.length-1 && board2[x+1][y] === "S")
   {
-    displayMessage("No!")
+    displayMessage("You can not place your ships next to each other! Choose an other field!", "red")
     return false
   }
-  else if (y > 0 && board2[x][y-1] === "X")
+  else if (y > 0 && board2[x][y-1] === "S")
   {
-    displayMessage("No!")
+    displayMessage("You can not place your ships next to each other! Choose an other field!", "red")
     return false
   }
-  else if (y < board2.length-1 && board2[x][y+1] === "X")
+  else if (y < board2.length-1 && board2[x][y+1] === "S")
   {
-    displayMessage("No!")
+    displayMessage("You can not place your ships next to each other! Choose an other field!", "red")
     return false
   }
   else
@@ -73,9 +74,31 @@ function resetGame() {
   displayBoard({boardnumber: 1,board: board});
 }
 
-function aiShoot(data) {
-  console.log(data);
-}
+let gameOver = 0;
+let aiShots = [];
+
+  function aiShoot() {
+    let x, y;
+    do {
+      x = Math.floor(Math.random() * 4);
+      y = Math.floor(Math.random() * 4);
+    } while (aiShots.includes(`${x},${y}`));
+
+    if (board2[x][y] === "S" && board[x][y] === "" && gameOver < 2) 
+    {
+      board[x][y] = "Hit";
+      displayBoard({ boardnumber: 1, board: board });
+      displayMessage("The AI hit your ship at " + String.fromCharCode(x+65) + y, "red");
+      aiShots.push (`${x},${y}`)
+      gameOver++
+    } else if (board[x][y] === "" ) 
+    {
+      board[x][y] = "M";
+      displayBoard({ boardnumber: 1, board: board });
+      displayMessage("The AI missed at " + String.fromCharCode(x+65) + y, "black");
+      aiShots.push (`${x},${y}`)
+    }
+  }
 
 displayBoard({boardnumber: 1,board: board});
 displayBoard({boardnumber: 2,board: board2});
